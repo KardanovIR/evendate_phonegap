ca142e590c23262872525d6fbe6ba4d5d38a3d59
Inal Kardanov <ir_kardanov@guu.ru>
2015-09-12 17:47:27 +0300

    minor changes

--- www/js/init.js
+++ www/js/init.js
@@ -124,8 +124,8 @@ myapp.init = (function () {
         // Initialize app
         fw7App = new Framework7({
             modalTitle: 'Evendate',
-            modalButtonCancel: 'ŒÚÏÂÌ‡',
-            modalPreloaderTitle: '«‡„ÛÁÍ‡ ...'
+            modalButtonCancel: '–û—Ç–º–µ–Ω–∞',
+            modalPreloaderTitle: '–ó–∞–≥—Ä—É–∑–∫–∞ ...'
         });
         $$ = Dom7;
         var fw7ViewOptions = {
@@ -153,13 +153,27 @@ function onDeviceReady(){
     var db_version = window.localStorage.getItem('db_version');
     __db = window.sqlitePlugin.openDatabase({name: CONTRACT.DB.NAME, location: 2});
 
-    window.sqlitePlugin.openDatabase({ name: 'hello-world.db', location: 2 }, function (db) {
-        db.executeSql("select length('tenletters') as stringlength", [], function (res) {
-            alert(JSON.stringify(res));
-            var string_slength = res.rows.item(0).stringlength;
-            alert('got stringlength: ' + string_length);
-        });
-    });
+    var db = window.sqlitePlugin.openDatabase("Database", "1.0", "Demo", -1);
+                db.transaction(function(tx) {
+              tx.executeSql('DROP TABLE IF EXISTS test_table');
+              tx.executeSql('CREATE TABLE IF NOT EXISTS test_table (id integer primary key, data text, data_num integer)');
+
+              tx.executeSql("INSERT INTO test_table (data, data_num) VALUES (?,?)", ["test", 100], function(tx, res) {
+              console.log("insertId: " + res.insertId + " -- probably 1"); // check #18/#38 is fixed
+              alert("insertId: " + res.insertId + " -- should be valid");
+
+                db.transaction(function(tx) {
+                  tx.executeSql("SELECT data_num from test_table;", [], function(tx, res) {
+                    console.log("res.rows.length: " + res.rows.length + " -- should be 1");
+                    alert("res.rows.item(0).data_num: " + res.rows.item(0).data_num + " -- should be 100");
+                  });
+                });
+
+              }, function(e) {
+                console.log("ERROR: " + e.message);
+              });
+            });
+
 
     //if (db_version != CONTRACT.DB.VERSION || CONTRACT.DB.VERSION == -1){
     //    updateDBScheme();
