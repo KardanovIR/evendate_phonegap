1ed819c432361455578efc48922d4ad8e805aa0a
Inal Kardanov <ir_kardanov@guu.ru>
2015-11-28 18:18:46 +0300

    Minor changes

--- www/js/init.js
+++ www/js/init.js
@@ -400,6 +400,9 @@ function onDeviceReady(){
 
     moment.locale("ru");
     //window.open = cordova.InAppBrowser.open;
+    if (window.analytics){
+        window.analytics.startTrackerWithId('UA-69300084-1');
+    }
     var db_version = window.localStorage.getItem('db_version');
     if (__os == 'win'){
         __db = window.openDatabase(CONTRACT.DB.NAME + '-' + makeid(), CONTRACT.DB.NAME, CONTRACT.DB.NAME, 5000, function(){
