8f74b1b09b46c1485acfb31a8f9f5c0730c0b286
Inal Kardanov <ir_kardanov@guu.ru>
2015-11-28 14:17:24 +0300

    Minor changes

--- www/js/pages/SubscriptionsPageController.js
+++ www/js/pages/SubscriptionsPageController.js
@@ -34,6 +34,7 @@ MyApp.pages.SubscriptionsPageController = function ($scope, $http) {
             $$page = $$('.profile-page-content');
         $$('.introjs-overlay, .introjs-helperLayer').hide();
         $$('.views.introjs-fixParent').removeClass('introjs-fixParent').addClass('patch-fixParent');
+        $$('.main-tabbar').removeClass('introjs-fixParent').addClass('patch-fixParent');
 
         $$page.on('infinite', function (){
           if ($$('#organizations').hasClass('active') && show_back_to_calendar == false && $$page.find('.button-filled-blue').length > 0){
