52d35920953afe1fae4dcf48f0e40c52d335cbb6
kardanovir <kardinal3094@gmail.com>
2017-05-09 22:54:12 +0300

    Few changes

--- www/js/init.js
+++ www/js/init.js
@@ -249,18 +249,30 @@ window.L = {
     }
 };
 
-var scanQR = function(){
+var scanQR = function () {
+    L.log('Trying barcode');
+    L.log('Plugin exists: ', cordova.plugins.barcodeScanner != undefined);
     cordova.plugins.barcodeScanner.scan(
         function (result) {
-            L.log("We got a barcode\n" +
-                "Result: " + result.text + "\n" +
-                "Format: " + result.format + "\n" +
-                "Cancelled: " + result.cancelled);
+            L.log(result);
         },
         function (error) {
-            L.log("Scanning failed: " + error);
+            L.log(error);
+        },
+        {
+            preferFrontCamera: true, // iOS and Android
+            showFlipCameraButton: true, // iOS and Android
+            showTorchButton: true, // iOS and Android
+            torchOn: true, // Android, launch with the torch switched on (if available)
+            prompt: "Place a barcode inside the scan area", // Android
+            resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
+            formats: "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
+            orientation: "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
+            disableAnimations: false, // iOS
+            disableSuccessBeep: false // iOS
         }
     );
+
 };
 
 var eventMethod = window.addEventListener ? "addEventListener" : "attachEvent";
